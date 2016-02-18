c80ea499b4c61c2b51eaf8471963301d357bef55
Ujjwal Gulecha <ujjwal.gulecha@gmail.com>
2016-02-18 00:08:25 -0800

    More Changes

--- simple-todos.js
+++ simple-todos.js
@@ -52,6 +52,42 @@ if (Meteor.isClient) {
     }
 
   });
+  // This code only runs on the client
+
+  Template.body.helpers({
+
+    tasks: function () {
+
+      // Show newest tasks at the top
+
+      return Tasks.find({}, {sort: {createdAt: -1}});
+
+    }
+
+  });
+ 
+
+  Template.task.events({
+
+    "click .toggle-checked": function () {
+
+      // Set the checked property to the opposite of its current value
+
+      Tasks.update(this._id, {
+
+        $set: {checked: ! this.checked}
+
+      });
+
+    },
+
+    "click .delete": function () {
+
+      Tasks.remove(this._id);
+
+    }
+
+  });
 }
 
 
